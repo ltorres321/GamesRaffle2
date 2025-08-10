32a7094a21b484eb3cddc7e9444c0d0cb3230507
Leo Torres <ltorres321@gmail.com>
2025-08-10 09:19:36 -0400

    Fix backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const logger = require('./utils/logger');
 const database = require('./config/database');
 const memoryCache = require('./config/memoryCache');
 const scheduledJobService = require('./services/scheduledJobService');
-const errorHandler = require('./middleware/errorHandler');
+const { errorHandler } = require('./middleware/errorHandler');
 const notFound = require('./middleware/notFound');
 
 // Import routes
